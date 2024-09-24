1b95af6dd42cff48e1343a89c671f7e9475b924b
Monika <maam092024@gmail.com>
2024-09-24 09:31:39 +0200

    xxc

---
+++ root folder/projekt 1/script.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+
+let lives = 5;
+
+lives++;
+
+const string = "monika monika monika";
+
+console.log(string);
+const boolean = true;
+
+console.log(boolean);
+
+const array = [
+  "mandag",
+  "tirsdag",
+  "onsdag",
+  "torsdag",
+  "fredag",
+  "lørdag",
+  "sondag",
+];
+
+const textElements = " detter_er_snake";
+
+document.querySelector("h1");
+
+console.log(array[2], array[6]);
