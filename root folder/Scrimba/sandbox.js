25699803dbf30a49d48ba7fd3200c7f362726b19
Monika <maam092024@gmail.com>
2024-09-24 09:32:04 +0200

    xxv

---
+++ root folder/Scrimba/sandbox.js
@@ -0,0 +1,19 @@
+alert("Come if you dare");
+
+console.log(1);
+
+let age = 25;
+let year = 2019;
+//comment
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+points = 40;
+console.log(points);
+
+var score = 76;
+console.log(score);
