44465fcd1b3ddf5662d621d7f47d86c4f072adb9
Monika <maam092024@gmail.com>
2024-10-02 14:37:53 +0200

    mom

--- newProject/Program.cs
+++ newProject/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace newProject;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography;
+
+namespace newProject;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+        Console.WriteLine("Hello, World!"); 
+
+int minTall = 38 ;
+Console.WriteLine(minTall);
+
+string tekst = "ha en fin dag";
+Console.WriteLine(tekst);
+
+double minDoubletall = 22.22 ;
+Console.WriteLine(minDoubletall);
+
+char førsteBokstav = 'A' ;
+Console.WriteLine(førsteBokstav);
+
+int[] minArray = { 1,2,3,4,5,6,7,8,9,10};
+for (int i = 0; i < minArray.Length; i++)
+{
+    Console.WriteLine(i);
+}
+
+string[] fruktArray = { "blåbær", "eple", "banan", "appelsin","kiwi", "pære", "mango" };
+foreach (string frukt in fruktArray)
+{ 
+    Console.WriteLine(frukt);
+}
+
+
+}
 }
